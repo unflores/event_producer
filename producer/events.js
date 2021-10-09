a2b3ec169ec0e0de56860759676f8f41da2c147d
Austin Flores <ctrl4ltdeleteme@gmail.com>
2021-10-09 15:07:20 +0200

    Split out events, make them sync. Push async code up the chain

---
+++ producer/events.js
@@ -0,0 +1,37 @@
+
+'use strict';
+
+module.exports = {
+  EVENTS: {
+    rider_signed_up: {
+      routing_key: 'rider.signup',
+      probability: 0.3
+    },
+    rider_updated_phone_number: {
+      routing_key: 'rider.phone_update',
+      probability: 0.05
+    },
+    ride_created: {
+      routing_key: 'ride.create',
+      probability: 0.2
+    },
+    ride_completed: {
+      routing_key: 'ride.completed',
+      probability: 0.2
+    }
+  },
+  ERRORS: {
+    wrong_schema: {
+      probability: 0.05
+    },
+    wrong_value: {
+      probability: 0.05
+    },
+    missing_publication: {
+      probability: 0.05
+    },
+    multiple_publication: {
+      probability: 0.1
+    }
+  }
+}
--- producer/index.js
+++ producer/index.js
@@ -2,11 +2,11 @@ const _ = require('lodash');
 const amqplib = require('amqplib');
 const logger = require('chpr-logger');
 const { ObjectID } = require('mongodb');
-
+const { EVENTS, ERRORS } = require('./events')
 /**
  * Several events are produced:
- * - rider signup
- * - rider phone update
+ * - actor signup
+ * - actor phone update
  * - ride created
  * - ride completed
  *
@@ -14,47 +14,16 @@ const { ObjectID } = require('mongodb');
  * - some events are sent twice
  * - some events are sent with wrong schema
  * - some events are sent with wrong value (ride amount = -2 €)
- * - some events are in the wrong order (ride create before rider signup)
+ * - some events are in the wrong order (ride create before actor signup)
  *
- * Special riders exist and send more events than others: these riders are the
+ * Special actors exist and send more events than others: these actors are the
  * keys of the test.
  */
 
 const AMQP_URL = process.env.AMQP_URL || 'amqp://guest:guest@localhost:5672';
 const EXCHANGE = 'events';
-const EVENTS = {
-  rider_signed_up: {
-    routing_key: 'rider.signup',
-    probability: 0.3
-  },
-  rider_updated_phone_number: {
-    routing_key: 'rider.phone_update',
-    probability: 0.05
-  },
-  ride_created: {
-    routing_key: 'ride.create',
-    probability: 0.2
-  },
-  ride_completed: {
-    routing_key: 'ride.completed',
-    probability: 0.2
-  }
-};
-const ERRORS = {
-  wrong_schema: {
-    probability: 0.05
-  },
-  wrong_value: {
-    probability: 0.05
-  },
-  missing_publication: {
-    probability: 0.05
-  },
-  multiple_publication: {
-    probability: 0.1
-  }
-};
-const SPECIAL_RIDERS = {
+
+const SPECIAL_ACTORS = {
   'Hubert Sacrin': {
     events: {
       rider_signed_up: {
@@ -102,23 +71,9 @@ const SPECIAL_RIDERS = {
 let client;
 
 /**
- * Full list of riders
+ * Full list of actors
  */
-const riders = new Map();
-
-/**
- * Initialize the AMQP connection and setup
- *
- * @returns {void}
- */
-async function init() {
-  logger.info('> RabbitMQ initialization');
-  client = await amqplib.connect(AMQP_URL);
-  client.channel = await client.createChannel();
-  await client.channel.assertExchange(EXCHANGE, 'topic', {
-    durable: true
-  });
-}
+const actors = new Map();
 
 /**
  * Publish message with possible error applied
@@ -129,7 +84,7 @@ async function init() {
  */
 async function publish(message) {
   const errors = _.mapValues(ERRORS, (error, key) => Math.random() < error.probability);
-  logger.debug({ errors }, 'Message publication applied errors');
+  logger.info({ errors }, 'Message publication applied errors');
   if (errors.multiple_publication) {
     await publish(message);
   }
@@ -152,12 +107,12 @@ async function publish(message) {
     // Apply wrong value to an always-present value
     message.payload.id = "undefined";
   }
-
-  logger.debug({
+  logger.info({
     exchange: EXCHANGE,
     routing_key: EVENTS[message.type].routing_key,
     message
   }, 'Message publications');
+
   client.channel.publish(
     EXCHANGE,
     EVENTS[message.type].routing_key,
@@ -168,142 +123,164 @@ async function publish(message) {
 }
 
 /**
- * A rider signed up
+ * A actor signed up
  *
- * @param {string} name rider name
+ * @param {string} name actor name
  */
-async function riderSignup(name) {
-  const rider = {
+function actorSignup(name) {
+  const actor = {
     id: ObjectID(),
     name: name || "John Doe"
   };
 
-  riders.set(rider.id, rider);
+  actors.set(actor.id, actor);
 
   // Message publication...
-  await publish({
+  return {
     type: 'rider_signed_up',
-    payload: rider
-  });
+    payload: actor
+  }
 }
 
 /**
- * A rider updated his phone number
+ * A actor updated his phone number
  *
- * @param {Object} rider
+ * @param {Object} actor
  */
-async function riderPhoneUpdate(rider) {
+function actorPhoneUpdate(actor) {
   // Message publication...
-  await publish({
+  return {
     type: 'rider_updated_phone_number',
     payload: {
-      ..._.pick(rider, 'id'),
+      ..._.pick(actor, 'id'),
       phone_number: `+336${Math.random().toString().slice(2,11)}`
     }
-  });
+  }
 }
 
 /**
- * A rider ordered a ride
+ * A actor ordered a ride
  *
- * @param {Object} rider
+ * @param {Object} actor
  */
-async function riderRideCreate(rider) {
+function actorRideCreate(actor) {
   const ride = {
     id: ObjectID(),
     amount: 3 + Math.floor(Math.random() * 30 * 100) / 100,
-    rider_id: rider.id
+    rider_id: actor.id
   };
 
-  // Attach the ride id to the rider for completed or canceled
-  riders.set(rider.id, {
-    ...rider,
+  // Attach the ride id to the actor for completed or canceled
+  actors.set(actor.id, {
+    ...actor,
     ride_id: ride.id
   });
+  logger.info("yo what'sup")
 
   // Message publication...
-  await publish({
+  return {
     type: 'ride_created',
     payload: ride
-  });
+  };
 }
 
 /**
- * A rider completed a ride
+ * A actor completed a ride
  *
- * @param {Object} rider
+ * @param {Object} actor
  */
-async function riderRideCompleted(rider) {
+function actorRideCompleted(actor) {
   const ride = {
-    id: rider.ride_id || ObjectID(),
+    id: actor.ride_id || ObjectID(),
     amount: 3 + Math.floor(Math.random() * 30 * 100) / 100,
-    rider_id: rider.id
+    rider_id: actor.id
   };
 
   // Message publication...
-  await publish({
+  return {
     type: 'ride_completed',
     payload: ride
-  });
+  };
 }
 
 /**
- * Rider lifecycle tic method
+ * Rider actions to be taken each tic
  *
- * @param {Object} rider
+ * @param {Object} actor
  */
-async function riderTic(rider) {
-  const probabilities = Object.assign({}, EVENTS, _.get(SPECIAL_RIDERS, `${rider.name}.events`, {}));
+async function actorActions(actor) {
+  const probabilities = Object.assign({}, EVENTS, _.get(SPECIAL_ACTORS, `${actor.name}.events`, {}));
 
   if (Math.random() < probabilities.rider_updated_phone_number.probability) {
-    riderPhoneUpdate(rider);
+    await publish(actorPhoneUpdate(actor));
   }
 
   if (Math.random() < probabilities.ride_created.probability) {
-    riderRideCreate(rider);
+    logger.info({event: actorRideCreate(actor)})
+    await publish(actorRideCreate(actor));
   }
 
   if (Math.random() < probabilities.ride_completed.probability) {
-    riderRideCompleted(rider);
+    await publish(actorRideCompleted(actor));
   }
 }
 
 /**
  * Global test tic method
  *
- * @param {Number} n number of riders
+ * @param {Number} n number of actors
  */
 async function tic(n) {
   logger.debug('tic');
-  if (n > riders.size && Math.random() < EVENTS.rider_signed_up.probability) {
-    riderSignup();
+
+  // if our iteration larger then the total actors
+  // then we can have a signup event
+  if (n > actors.size && Math.random() < EVENTS.rider_signed_up.probability) {
+    actorSignup();
   }
 
-  // Special riders creation
-  for (const name in SPECIAL_RIDERS) {
-    if (Math.random() < SPECIAL_RIDERS[name].events.rider_signed_up.probability) {
-      riderSignup(name);
+  // Special actors creation
+  // Every iteration we can have a special actor sign up
+  for (const name in SPECIAL_ACTORS) {
+    if (Math.random() < SPECIAL_ACTORS[name].events.rider_signed_up.probability) {
+      actorSignup(name);
 
-      // Unique special rider creation:
-      SPECIAL_RIDERS[name].events.rider_signed_up.probability = 0;
+      // Unique special actor creation:
+      SPECIAL_ACTORS[name].events.rider_signed_up.probability = 0;
     }
   }
 
+  // For every iteration our actors to run their events
   const tics = [];
-  riders.forEach(rider => tics.push(riderTic(rider)));
-  logger.debug({ tics_length: tics.length }, 'Riders tic length');
-  logger.info({ tics: tics.length }, 'Number of riders tics');
+  actors.forEach(actor => tics.push(actorActions(actor)));
+  logger.info({ tics_length: tics.length }, 'Riders tic length');
+  logger.info({ tics: tics.length }, 'Number of actors tics');
   await Promise.all(tics);
 }
 
 /**
+ * Initialize the AMQP connection and setup
+ *
+ * @returns {void}
+ */
+async function init() {
+  logger.info('> RabbitMQ initialization');
+  client = await amqplib.connect(AMQP_URL);
+  client.channel = await client.createChannel();
+  await client.channel.assertExchange(EXCHANGE, 'topic', {
+    durable: true
+  });
+}
+
+/**
  * Main function of the script
- * @param {number} [n=10] Number of riders to start
+ * @param {number} [n=10] Number of actors to start
  * @param {number} [interval=1000] Time interval (ms) before increasing the messages rate
  */
 async function main(n = 10, interval = 1000) {
-  logger.info('> Riders initialization...');
+
   await init();
+
   while (true) {
     await Promise.all([
       tic(n),
